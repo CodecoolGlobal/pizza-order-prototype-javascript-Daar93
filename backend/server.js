c9c6a14e78bea70d4677f4f22d00334c269b5684
Lilpeanutz <shengkaixia@gmx.at>
2023-04-04 13:36:15 +0200

    save commit

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,18 @@ const express = require("express");
 const path = require("path");
 const fileReaderAsync = require("./fileReader.js");
 const filePath = path.join(`${__dirname}/pizzas.json`);
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const fs = require("fs");
 const cons = require("consolidate");
-const { response } = require("express");
 const router = express.Router();
-const pizzasRaw = fs.readFileSync(`${__dirname}/pizzas.json`, "utf-8");
-const pizzas = JSON.parse(pizzasRaw);
-const allergeneRaw = fs.readFileSync(`${__dirname}/allergene.json`, "utf-8")
+const pizzasRaw = fs.readFileSync(`${__dirname}/pizzas.json`, "utf8");
+const pizzas = JSON.parse(pizzasRaw)
+const allergeneRaw = fs.readFileSync(`${__dirname}/allergene.json`, "utf8")
 const allergene = JSON.parse(allergeneRaw)
 
-
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(express.static('public'));
@@ -24,38 +22,25 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = 9001;
 
-console.log(`${__dirname}/../frontend/index.html`);
+console.log(`${__dirname}/../views/index.html`);
 
 app.engine("html", cons.swig);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 
+app.get("/pizza/list",(req, res) => {
+    res.render(path.join(`${__dirname}/../views/index.html`));
+});
+
 app.get("/api/pizza",(req, res) => {
-    res.json(pizzas)
-    res.end()
+    res.json(pizzas);
 });
 
 app.get("/api/allergene", (req, res) => {
     res.json(allergene)
-    res.end()
 })
 
-
-app.get("/pizza/list",(req, res) => {
-
-    fetch(`http://127.0.0.1:9001/api/pizza`)
-    .then(response => response.json())
-    .then(data => {
-        const root = document.querySelector("#root")
-
-    })
-    .catch(error => {
-        console.log(error);
-    })
-});
-
-
-app.use(express.static(`${__dirname}/../frontend`));
+app.use(express.static(`${__dirname}/../views`));
 
 console.log(__dirname);
 // router
@@ -77,6 +62,8 @@ console.log(__dirname);
 //   req.user = JSON.stringify(packagesJS.packages[id - 1]);
 //   next();
 // });
+
+
 
 app.listen(port, () => console.log(`http://127.0.0.1:${port}/api/pizza`));
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
-console.log("script here")
+fetch("http://127.0.0.1:9001/api/pizza")
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+})
+// const fs = require("fs");
+
+// const pizzas = fs.readFileSync("./backend/pizzas.json", "utf8");
+// console.log(pizzas);
+
+console.log("HAllooo");
+
+// const root = document.getElementById("root");
+
+// root.insertAdjacentHTML("beforeend", "<button>");
