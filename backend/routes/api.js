df899ca6d25b703c37aa88623af37306b4a21d07
Lilpeanutz <shengkaixia@gmx.at>
2023-04-05 09:24:36 +0200

    routes folder created

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,12 @@
 const express = require("express")
+const path = require("path");
+const filePath = path.join(`${__dirname}/pizzas.json`);
 const router = express.Router()
+const fs = require("fs")
+const pizzasRaw = fs.readFileSync(`${__dirname}/../pizzas.json`, "utf8");
+const pizzas = JSON.parse(pizzasRaw)
+const allergeneRaw = fs.readFileSync(`${__dirname}/../allergene.json`, "utf8")
+const allergene = JSON.parse(allergeneRaw)
 
 router.get("/pizza",(req, res) => {
     res.json(pizzas);
@@ -8,3 +15,5 @@ router.get("/pizza",(req, res) => {
 router.get("/allergene", (req, res) => {
     res.json(allergene)
 })
+
+module.exports = router
