4e2d7fe131a6c5535b08e6b0855640668bac3a3b
Mehmet <mehmet.ali.yilmaz1711@gmail.com>
2023-04-05 12:04:39 +0200

    adde styling

--- backend/views/frontend/script.js
+++ backend/views/frontend/script.js
@@ -1,5 +1,49 @@
 const root = document.getElementById("root");
 
+function insertElementTo(element, elementToInsert) {
+    element.appendChild(elementToInsert);
+};
+
+function createHeader() {
+    const header = document.createElement("div");
+    header.setAttribute("id", "header");
+
+    const h1 = document.createElement("h1");
+    h1.innerText = "Mario's Pizza";
+    header.appendChild(h1);
+
+    return header;
+};
+
+function createContainer() {
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    container.appendChild(row);
+
+    return container;
+};
+
+function createBackgroundImage() {
+    const backgroundImage = document.createElement("img");
+    backgroundImage.setAttribute("src", "/frontend/images/background.jpeg");
+    backgroundImage.setAttribute("id", "background-image");
+
+    return backgroundImage;
+};
+
+function insertHtmlTree() {
+    insertElementTo(root, createHeader());
+    insertElementTo(root, createContainer());
+    insertElementTo(root, createBackgroundImage());
+};
+
+insertHtmlTree();
+
+const row = document.querySelector(".row");
+
 function createAllergens(container) {
     const allergens = document.createElement("h6");
     allergens.setAttribute("id", "allergens");
@@ -18,25 +62,23 @@ function createCard(pizzas, nameOfPizza, pizzaPrice, index) {
     if(index === 3) {
         root.insertAdjacentHTML("beforeend", "<br>");
     }
-    
-    const cardDiv = document.createElement("div");
-    cardDiv.setAttribute("id", `card`);
-    cardDiv.style.backgroundImage = `url(${pizzas.img})`;
 
-    const container = document.createElement("div");
-    container.setAttribute("id", "container");
-    cardDiv.appendChild(container);
+    const pizza = document.createElement("div");
+    pizza.setAttribute("id", "pizza");
+    pizza.classList.add("col-sd-12");
+    pizza.classList.add("col-md-4");
+    row.appendChild(pizza);
 
     const pizzaName = document.createElement("h4");
     pizzaName.setAttribute("id", "pizza-name");
     pizzaName.innerText = nameOfPizza;
-    container.appendChild(pizzaName);
+    pizza.appendChild(pizzaName);
 
     const ingredientsString = pizzas.ingredients.reduce((string, ingredient, index) => {
         return string + ingredient + "<br>"
     }, "");
-    console.log(ingredientsString);
-    container.insertAdjacentHTML("beforeend", `<h6>${ingredientsString}</h6>`);
+
+    pizza.insertAdjacentHTML("beforeend", `<h6>${ingredientsString}</h6>`);
 
     const allergens = pizzas.allergens.reduce((string, allergen, index) => {
         if(index === 0) {
@@ -46,16 +88,17 @@ function createCard(pizzas, nameOfPizza, pizzaPrice, index) {
         }
     }, "");
 
-    container.insertAdjacentHTML("beforeend", allergens);
+    pizza.insertAdjacentHTML("beforeend", allergens);
 
     const price = document.createElement("h4");
     price.setAttribute("id", "price");
     price.innerText = pizzaPrice + "$";
-    container.appendChild(price);
+    pizza.appendChild(price);
 
     const button = document.createElement("button");
     button.innerText = "Order";
-    cardDiv.appendChild(button);
+    button.setAttribute("id", index + 1);
+    pizza.appendChild(button);
 
     button.addEventListener("click", event => {
         console.log("CLICK");
@@ -73,19 +116,17 @@ function createCard(pizzas, nameOfPizza, pizzaPrice, index) {
             });
     });
 
-    return cardDiv;
+    return pizza;
 };
 
-function insertElementTo(element, elementToInsert) {
-    element.appendChild(elementToInsert);
-};
+
 
 fetch("http://127.0.0.1:9001/api/pizza")
     .then(response => response.json())
     .then(data => {
         console.log(data);
         data.pizzas.map((pizza, index) => {
-            insertElementTo(root, createCard(pizza, pizza.name, pizza.price, index));
+            insertElementTo(row, createCard(pizza, pizza.name, pizza.price, index));
         });
     });
 
