d9353d25934b7c3bff938a10716ea51fa7c6ec61
Jules Pabst <ju.pabst@gmail.com>
2023-04-06 17:33:13 +0200

    added pizza numbers

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,26 +2,38 @@ const express = require("express");
 const path = require("path");
 const filePath = path.join(`${__dirname}/pizzas.json`);
 const router = express.Router();
-const fs = require("fs")
+const fs = require("fs");
 const pizzasRaw = fs.readFileSync(`${__dirname}/../pizzas.json`, "utf8");
 const allergeneRaw = fs.readFileSync(`${__dirname}/../allergene.json`, "utf8");
 const ordersRaw = fs.readFileSync(`${__dirname}/../orders.json`, "utf8");
 
-router.get("/pizza",(req, res) => {
-    res.json(pizzasRaw);
+router.get("/pizza", (req, res) => {
+  res.json(pizzasRaw);
 });
 
 router.get("/allergene", (req, res) => {
-    res.json(allergeneRaw);
+  res.json(allergeneRaw);
 });
 
 router.get("/order/list", (req, res) => {
-    console.log(ordersRaw)
-    res.json(ordersRaw);
+  console.log(ordersRaw);
+  res.json(ordersRaw);
 });
 
 router.get("/order", (req, res) => {
-    res.render(path.join(`${__dirname}/../views/userInput.html`));
+  res.render(path.join(`${__dirname}/../views/userInput.html`));
+});
+
+router.get("/orderInformation", (req, res) => {
+  res.json(orderInformationRaw);
+});
+
+router.post("/", (req, res) => {
+  // req.body = {"name": "Memo"};
+  // console.log(req.body);
+  req.body = JSON.stringify(req.body);
+  fs.writeFile(path.join(`${__dirname}/../orders.json`), req.body, (err) => {});
+  res.json(req.body);
 });
 
 module.exports = router;
--- backend/views/frontend/userInput.js
+++ backend/views/frontend/userInput.js
@@ -9,83 +9,165 @@
 // const ordersRaw = fs.readFileSync(`${__dirname}/../orders.json`, "utf8");
 // const order = JSON.parse(ordersRaw);
 
-// console.log(orderedPizzasJS);
-
 const root = document.querySelector("#root");
-console.log(root);
+const amountOfPizzas = {};
 
 function createHeader() {
-    const header = document.createElement("div");
-    header.setAttribute("id", "header");
+  const header = document.createElement("div");
+  header.setAttribute("id", "header");
 
-    const h1 = document.createElement("h1");
-    h1.innerText = "Mario's πzza";
-    header.appendChild(h1);
+  const h1 = document.createElement("h1");
+  h1.innerText = "Mario's πzza";
+  header.appendChild(h1);
 
-    // const button = document.createElement("button");
-    // button.innerText = "Your Order!";
-    // button.setAttribute("id", "order-button");
-    // header.appendChild(button);
+  // const button = document.createElement("button");
+  // button.innerText = "Your Order!";
+  // button.setAttribute("id", "order-button");
+  // header.appendChild(button);
 
-
-    return header;
-};
+  return header;
+}
 
 function createInput(idName, placeholder) {
-    const input = document.createElement("input");
-    input.setAttribute("id", idName);
-    input.classList.add("input-fields");
-    input.setAttribute("placeholder", placeholder);
+  const input = document.createElement("input");
+  input.setAttribute("id", idName);
+  input.setAttribute("name", idName);
+  input.classList.add("input-fields");
+  input.setAttribute("placeholder", placeholder);
 
-    return input;
+  return input;
+}
+
+function createSubmit(idName, placeholder) {
+  const input = document.createElement("input");
+  input.setAttribute("id", idName);
+  input.setAttribute("type", "submit");
+  input.setAttribute("value", "Send");
+  input.classList.add("input-fields");
+  input.classList.add("add-to-order-button");
+  input.innerText = "submit";
+  input.setAttribute("placeholder", placeholder);
+
+  return input;
 }
 
 function createTitle() {
-    const h1 = document.createElement("h1");
-    h1.classList = "title";
-    h1.innerText = "Your Order";
+  const h1 = document.createElement("h1");
+  h1.classList = "title";
+  h1.innerText = "Your Order";
 
-    return h1;
-};
+  return h1;
+}
+
+function createPizzas() {
+  const h2 = document.createElement("h1");
+  h2.setAttribute("id", "orderedPizzas");
+
+  h2.innerText = "Your Order";
+
+  return h2;
+}
 
 function createBackgroundImage() {
-    const backgroundImage = document.createElement("img");
-    backgroundImage.setAttribute("src", "/frontend/images/background.jpeg");
-    backgroundImage.setAttribute("id", "background-image");
+  const backgroundImage = document.createElement("img");
+  backgroundImage.setAttribute("src", "/frontend/images/background.jpeg");
+  backgroundImage.setAttribute("id", "background-image");
 
-    return backgroundImage;
-};
+  return backgroundImage;
+}
 
 function insertInputFieldsTo(element) {
-    element.appendChild(createTitle());
-    element.appendChild(createInput("name", "Name"));
-    element.appendChild(createInput("email", "Email"));
-    element.appendChild(createInput("city", "City"));
-    element.appendChild(createInput("street", "Street"));
+  element.appendChild(createTitle());
+  element.appendChild(createPizzas());
+  element.appendChild(createInput("name", "Name"));
+  element.appendChild(createInput("email", "Email"));
+  element.appendChild(createInput("city", "City"));
+  element.appendChild(createInput("street", "Street"));
+  element.appendChild(createSubmit("submit", "submit"));
 }
 
 function createFrom() {
-    const form = document.createElement("form");
-    form.classList.add("formular");
+  const form = document.createElement("form");
+  form.classList.add("formular");
 
-    insertInputFieldsTo(form);
+  insertInputFieldsTo(form);
 
-    return form;
-};
+  return form;
+}
 
 function insertElementTo(element, elementToInsert) {
-    element.appendChild(elementToInsert);
-};
+  element.appendChild(elementToInsert);
+}
 
 insertElementTo(root, createHeader());
-insertElementTo(root, createFrom())
+insertElementTo(root, createFrom());
 insertElementTo(root, createBackgroundImage());
 
+function countNumberOfPizzas(orderedPizzas, comparePizza) {
+  let filteredPizzas = orderedPizzas.filter((pizza) => pizza === comparePizza);
+  let numberOfPizzas = filteredPizzas.length;
+  return numberOfPizzas;
+}
+
+function countPizzas(orderedPizzas) {
+  const setOfPizzas = new Set(orderedPizzas);
+  const uniquePizzas = Array.from(setOfPizzas);
+
+  for (let kindOfPizza of uniquePizzas) {
+    let numberOfPizzas = countNumberOfPizzas(orderedPizzas, kindOfPizza);
+    amountOfPizzas[kindOfPizza.name] = numberOfPizzas;
+  }
+  return amountOfPizzas;
+}
 
 fetch("http://127.0.0.1:9001/api/order/list")
-    .then(data => data.json())
-    .then(data => {
-        const orderedPizzas = JSON.parse(data);
+  .then((data) => data.json())
+  .then((data) => {
+    const pizzasElement = document.querySelector("#orderedPizzas");
+    const orderedPizzas = JSON.parse(data);
+    const pizzaTextWithCurlyBraces = JSON.stringify(
+      countPizzas(orderedPizzas)
+    ).replace(/"/g, "");
+    const pizzaTextWithCurlyBrace = pizzaTextWithCurlyBraces.replace(/{/g, "");
+    const pizzaTextWithOutCurlyBracesWithoutWhiteSpace =
+      pizzaTextWithCurlyBrace.replace(/}/g, "");
+    const pizzaTextWithOutCurlyBracesWithWhiteSpace =
+      pizzaTextWithOutCurlyBracesWithoutWhiteSpace.replace(/,/g, ", ");
+    const pizzaTextWithOutCurlyBracesWithOutDoubleWhiteSpace =
+      pizzaTextWithOutCurlyBracesWithWhiteSpace.replace(/:/g, ": ");
 
-        console.log(orderedPizzas);
-    })
+    pizzasElement.innerHTML =
+      pizzaTextWithOutCurlyBracesWithOutDoubleWhiteSpace;
+  });
+
+const submitElement = document.querySelector("#submit");
+const formElement = document.querySelector(".formular");
+const nameElement = document.querySelector("#name");
+const emailElement = document.querySelector("#email");
+const cityElement = document.querySelector("#city");
+const streetElement = document.querySelector("#street");
+const personalInformation = {};
+
+formElement.onsubmit = function (event) {
+  event.preventDefault();
+  const name = nameElement.value;
+  const email = emailElement.value;
+  const city = cityElement.value;
+  const street = streetElement.value;
+
+  personalInformation.pizzas = amountOfPizzas;
+  personalInformation.contact.name = name;
+  personalInformation.contact.email = email;
+  personalInformation.contact.city = city;
+  personalInformation.contact.street = street;
+
+  fetch("http://127.0.0.1:9001/orderInformation", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(personalInformation),
+  })
+    .then((response) => response.json())
+    .then((data) => {});
+};
